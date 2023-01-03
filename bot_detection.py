33e66d101ef1bca0d51a3d717790cffbb9bef166
Xiao Meng <34294648+mengxiao2000@users.noreply.github.com>
2023-01-03 20:22:56 +0800

    Update bot_detection.py

--- bot_detection.py
+++ bot_detection.py
@@ -6,12 +6,13 @@ from PIL import Image
 import requests 
 
 st.set_page_config(
-    page_title="微博机器人识别",
+    page_title="Bot Finder",
     page_icon="🤖️",
 #     layout="wide",
 )
 
-st.markdown('# <center> 🤖️ 微博机器人识别 </center>', unsafe_allow_html=True)
+st.markdown('# <center> 🤖️ Bot Finder</center>', unsafe_allow_html=True)
+st.markdown(' <center> 微博社交机器人探测器 🛸 </center>', unsafe_allow_html=True)
 st.write("\n  ")
 st.write("\n  ")
 st.write("\n  ")
@@ -52,7 +53,7 @@ select = col2_search.radio(
 if select == '昵称':
     st.text_input('请输入准确的用户昵称 (例如:人民日报)',key="user_name",help='根据用户昵称查找的原理是根据昵称搜索用户，对搜索到的第一个用户进行识别。')
 else:
-    st.text_input("输入用户ID (例如:6374435213)", key="uid")
+    st.text_input("输入用户UID (例如:6374435213或https://weibo.com/u/6374435213)", key="uid")
 
     
 ###########
@@ -95,7 +96,7 @@ if st.button('🚀识别'):
             
     else:
         if (st.session_state.uid).strip() == "":
-            st.error('用户ID不能为空！', icon="🚨")
+            st.error('用户UID不能为空！', icon="🚨")
         else:
             user_data = crawl_info.crawl_info((st.session_state.uid).strip())
             user_data = predict_bot(user_data)
@@ -115,7 +116,7 @@ with tab1:
     st.markdown(" **社交机器人**(social bot)是活跃在社交媒体中，由自动化算法操纵的能够模仿人类行为、自动生成内容并和人类账号产生互动的社交媒体账号。在国内舆论场中，社交机器人已经被发现操纵娱乐生态，干扰算法评价体系。社交机器人能够营造虚假人气，通过群体压力和网络传染，使复杂的社交媒体环境更加不确定。")
     
 with tab2:
-    st.markdown('该工具利用微博可公开获取的社交账号信息作为实验数据集，基于XGboost模型识别微博平台中的社交机器人。从新浪微博社交机器人商家处购买了不同智能水平的机器人账号共274个，再从亲友处、非广告的微博评论中以及具有代表性的公众人物、机构账号中人工筛选出人类账号共281个，利用爬虫采集这两类账号的公开数据，构建了账号原始数据集。参考以往文献在社交机器人识别方面使用的账号级别、内容级别等方面的特征，对每一个账号共抽取了22个特征，包括用户的性别、等级、是否认证、关注数、粉丝数、关注数和粉丝数之比、发布内容的原创率、发布频率等。选择随机森林、XGboost、决策树、支持向量机、Logistic回归、感知机、KNN和朴素贝叶斯作为二分类机器学习算法，构建社交机器人检测模型。为比较模型预测性能，采用5折交叉验证，XGBoost模型在测试集上预测性能最优（准确率：94.596%，召回率：95.545%）。')
+    st.markdown('该工具利用微博可公开获取的社交账号信息作为实验数据集，基于XGboost模型识别微博平台中的社交机器人。从新浪微博社交机器人商家处购买了不同智能水平的机器人账号共274个，再从亲友处、非广告的微博评论中以及具有代表性的公众人物、机构账号中人工筛选出人类账号共281个，利用爬虫采集这两类账号的公开数据，构建了账号原始数据集。参考以往文献在社交机器人识别方面使用的账号级别、内容级别等方面的特征，对每一个账号共抽取了22个特征，包括用户的性别、等级、是否认证、关注数、粉丝数、关注数和粉丝数之比、发布内容的原创率、发布频率等。选择随机森林、XGboost、决策树、支持向量机、Logistic回归、感知机、KNN和朴素贝叶斯作为二分类机器学习算法，构建社交机器人检测模型。为比较模型预测性能，采用5折交叉验证，XGBoost模型在测试集上预测性能最优（准确率：94.60%，召回率：95.55%）。')
     st.markdown('注：模型预测结果仅表明该账号是否有类似社交机器人的行为，预测结果仅供参考。')
     st.markdown('获取详情信息，请联系mengxiaocntc@163.com')
     
