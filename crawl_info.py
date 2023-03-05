911ac9da7a25cad0edc5248a0482557387835791
Xiao Meng <34294648+mengxiao2000@users.noreply.github.com>
2023-03-05 18:58:38 +0800

    Update crawl_info.py

--- crawl_info.py
+++ crawl_info.py
@@ -69,6 +69,7 @@ def get_user_weibo(uid=6374435213, proxies=None):
                 x_info = pd.DataFrame(x['data']['cards'])
                 #time.sleep(1)
                 x_ = pd.concat([x_, x_info], axis=0)
+                
     # 整理微博信息
     for line in x_.query('card_type == 9')['mblog']:
         created_at = pd.to_datetime(line['created_at'])
@@ -104,37 +105,50 @@ def get_user_weibo(uid=6374435213, proxies=None):
 
 def get_user_info(uid=6374435213):
 
-    url = 'https://m.weibo.cn/api/container/getIndex?mod=pedit_more%3Fmod%3Dpedit_more&jumpfrom=weibocom&containerid=100505' + str(uid) #6374435213
-    res = requests.get(url, timeout=1).text
-    info = json.loads(res)
-    
-    uid = info['data']['userInfo']['id']
-    screen_name = info['data']['userInfo']['screen_name']
+    #url = 'https://m.weibo.cn/api/container/getIndex?mod=pedit_more%3Fmod%3Dpedit_more&jumpfrom=weibocom&containerid=100505' + str(uid) #6374435213
+    #https://m.weibo.cn/api/container/getIndex?&containerid=1005056308541867
+    try:
+        url = 'https://m.weibo.cn/api/container/getIndex?&containerid=100505' + str(uid) #6374435213
 
-    verified = info['data']['userInfo']['verified']
-    verified_type = info['data']['userInfo']['verified_type']
+        headers={'user-agent':'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'}
 
-    urank = info['data']['userInfo']['urank']
-    mbrank = info['data']['userInfo']['mbrank']
+        res = requests.get(url, timeout=1).text
+        info = json.loads(res)
+ 
+        uid = uid
+        screen_name = info['data']['userInfo']['screen_name']
 
-    statuses_count = info['data']['userInfo']['statuses_count']
-    follow_count = info['data']['userInfo']['follow_count']
-    followers_count = info['data']['userInfo']['followers_count']
-    
-    gender =  info['data']['userInfo']['gender']
-    description = info['data']['userInfo']['description']
+        verified = info['data']['userInfo']['verified']
+        verified_type = info['data']['userInfo']['verified_type']
 
-    profile_image_url = info['data']['userInfo']['profile_image_url']
-    
-    df_ = pd.DataFrame([[uid, screen_name, verified, verified_type, urank, mbrank, statuses_count, follow_count, followers_count, gender, description, profile_image_url]], columns = ['uid', 'screen_name', 'verified', 'verified_type', 'urank', 'mbrank', 'statuses_count', 'follow_count', 'followers_count', 'gender', 'description', 'profile_image_url'])
+        urank = info['data']['userInfo']['urank']
+        mbrank = info['data']['userInfo']['mbrank']
+
+        statuses_count = info['data']['userInfo']['statuses_count']
+        follow_count = info['data']['userInfo']['follow_count']
+        followers_count = info['data']['userInfo']['followers_count']
+
+        gender =  info['data']['userInfo']['gender']
+        description = info['data']['userInfo']['description']
+
+        profile_image_url = info['data']['userInfo']['profile_image_url']
+
+        df_ = pd.DataFrame([[uid, screen_name, verified, verified_type, urank, mbrank, statuses_count, follow_count, followers_count, gender, description, profile_image_url]], columns = ['uid', 'screen_name', 'verified', 'verified_type', 'urank', 'mbrank', 'statuses_count', 'follow_count', 'followers_count', 'gender', 'description', 'profile_image_url'])
+        
+    except Exception as e:
+
+        df_ = pd.DataFrame([[np.NAN for i in range(12)]], columns = ['uid', 'screen_name', 'verified', 'verified_type', 'urank', 'mbrank', 'statuses_count', 'follow_count', 'followers_count', 'gender', 'description', 'profile_image_url'])
+        df_['uid'] = uid
+
     df_.to_csv('user_info.csv',mode='w', index=None)
 
     
 
 # 分析微博内容
 def wan_transfer(text):
-    text = str(text)
+    
     try:
+        text = str(text)
         if '万' in text:
             num = float(text.strip('万'))
             return int(num*10000)
@@ -185,7 +199,7 @@ def cal_origin(csv_name):
         return pd.DataFrame([[origin_rate, like_num, forward_num, comment_num, post_freq, post_location, content_length, content_std, richness, hashtag, at]], columns=['origin_rate','like_num','forward_num','comment_num','post_freq', 'post_location', 'content_length', 'content_std', 'richness','hashtag', 'at'])
     
     except Exception as e:
-        st.write(e)
+
         return pd.DataFrame([[np.NAN for i in range(11)]],columns=['origin_rate','like_num','forward_num','comment_num','post_freq', 'post_location', 'content_length', 'content_std','richness','hashtag', 'at'])
 
 
@@ -198,36 +212,45 @@ def nickname_digit(s):
 
 # 提取用户属性
 def user_attr(data):
-    data['verified'] = data['verified'].map({True:1,False:0})
+    try:
+        data['verified'] = data['verified'].map({True:1,False:0})
 
-#     data['sunshine_credit_level'] = data['sunshine_credit_level'].map({'信用极低':0,'信用较低':1,'信用一般':2,'信用较好':3,'信用极好':4})
+    #     data['sunshine_credit_level'] = data['sunshine_credit_level'].map({'信用极低':0,'信用较低':1,'信用一般':2,'信用较好':3,'信用极好':4})
 
-#    data['school'] = data['school'].apply(lambda x: 1 if pd.notna(x) else 0)
+    #    data['school'] = data['school'].apply(lambda x: 1 if pd.notna(x) else 0)
 
-#     data['location'] = data['location'].apply(lambda x: 1 if pd.notna(x) else 0)
+    #     data['location'] = data['location'].apply(lambda x: 1 if pd.notna(x) else 0)
 
-    data['gender'] = data['gender'].map({'m':1,'f':0})
+        data['gender'] = data['gender'].map({'m':1,'f':0})
 
-#     data['created_at'] = pd.to_datetime(data['created_at'])
-#     data['created_year'] = data['created_at'].apply(lambda x: x.year)
+    #     data['created_at'] = pd.to_datetime(data['created_at'])
+    #     data['created_year'] = data['created_at'].apply(lambda x: x.year)
 
-    data['description'] = data['description'].apply(lambda x: 0 if  x=='暂无简介' else 1)
+        data['description'] = data['description'].apply(lambda x: 0 if  x=='暂无简介' else 1)
 
-#     data['birthday_date'] = data['birthday'].apply(lambda x: 1 if pd.notna(x) else 0)
+    #     data['birthday_date'] = data['birthday'].apply(lambda x: 1 if pd.notna(x) else 0)
 
-    # 粉丝数、微博数、关注数
-    data['follow_count'] = data['follow_count'].apply(lambda x: wan_transfer(x))
-    data['followers_count'] = data['followers_count'].apply(lambda x: wan_transfer(x))
-    data['statuses_count'] = data['statuses_count'].apply(lambda x: wan_transfer(x))
+        # 粉丝数、微博数、关注数
+        data['follow_count'] = data['follow_count'].apply(lambda x: wan_transfer(x))
+        data['followers_count'] = data['followers_count'].apply(lambda x: wan_transfer(x))
+        data['statuses_count'] = data['statuses_count'].apply(lambda x: wan_transfer(x))
 
-    data['followers_follow'] = data['followers_count']/(data['follow_count']+1)
-    data['statuses_follow'] = data['statuses_count']/(data['follow_count']+1)
+        data['followers_follow'] = data['followers_count']/(data['follow_count']+1)
+        data['statuses_follow'] = data['statuses_count']/(data['follow_count']+1)
+
+        data['name_digit'] = data['screen_name'].apply(lambda x:1 if nickname_digit(x)>= 1 else 0)
+        data['name_length'] = data['screen_name'].apply(lambda x: len(x))
     
-    data['name_digit'] = data['screen_name'].apply(lambda x:1 if nickname_digit(x)>= 1 else 0)
-    data['name_length'] = data['screen_name'].apply(lambda x: len(x))
     
-    return data
+        return data
+    except:
+        data['followers_follow'] = np.NAN
+        data['statuses_follow'] = np.NAN
 
+        data['name_digit'] =  np.NAN
+        data['name_length'] =  np.NAN
+     
+        return data
 
 ##################
 # 抓取信息并分析内容
@@ -243,15 +266,17 @@ def crawl_info(uid):
         #分析内容
         df_uid = cal_origin(str(uid)+'.csv')
         df_uid['uid'] = int(uid)
-
+        
         user_info = pd.read_csv('user_info.csv')
+        
         data = user_attr(user_info)
-
+        
         #合并微博发布特征与用户属性特征
         user_data = pd.merge(left=data, right=df_uid, on='uid', how='left')
+        
+        
         return user_data
     except Exception as e:
+
         pass
-        #st.write(e)
-        
 
